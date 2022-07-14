b1dc9d473818b5c357f10b786b0260691e412210
Dillon Harless <dillonharless@gmail.com>
2022-07-14 13:30:20 -0400

    fixed logo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,4 @@
-import LogoTitle from '../../assets/images/logo-s.png';
+import LetterD from '../../assets/images/LetterD.png';
 import { Link } from 'react-router-dom';
 import './index.scss';
 import { useState } from 'react';
@@ -7,29 +7,32 @@ import AnimatedLetters from '../AnimatedLetters'
 const Home = () => {
 
     const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+    const nameArray = ['i', 'l', 'l', 'o', 'n']
     const jobArray  = ['w','e','b',' ','d','e','v','e','l','o','p','e','r','.']
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i</span>
-                <br />
-                <span className={`${letterClass} _13`}>I</span>
-                <span className={`${letterClass} _14`}>'m</span>
-                <img src={LogoTitle} alt="developer" />
-                <AnimatedLetters 
-                    letterClass={letterClass}
-                    strArray={nameArray}
-                    idx={15}
-                />
-                <br />
-                <AnimatedLetters 
-                    letterClass={letterClass}
-                    strArray={jobArray}
-                    idx={22}
-                />
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i</span>
+                    <span className={`${letterClass} _13`}>,</span>
+                    <br />
+                    <span className={`${letterClass} _14`}>I</span>
+                    <span className={`${letterClass} _15`}>'m</span>
+                    <img src={LetterD} alt="developer" />
+                    <AnimatedLetters 
+                        letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={16}
+                    />
+                    <br />
+                    <AnimatedLetters 
+                        letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={23}
+                    />
+                </h1>
                 <h2>Fullstack Developer / AWS Solutions Architect</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
 
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,6 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
-import LogoS from '../../assets/images/logo-s.png'
+import LogoD from '../../assets/images/logo-d.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
@@ -11,7 +11,7 @@ const Sidebar = () => {
     return (
         <div className='nav-bar'>
             <Link className='logo' to="/">
-                <img src={LogoS} alt="logo" />
+                <img src={LogoD} alt="logo" />
                 <img className='sub-logo' src={LogoSubtitle} alt="slobodan" />
             </Link>
             <nav>
